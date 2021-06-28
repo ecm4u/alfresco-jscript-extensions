38c9f4f00bed7bec7a419ae3cd8eb3dcbc78e99d
Heiko Robert <heiko.robert@ecm4u.de>
2021-06-28 16:34:15 +0200

    fix imports

--- src/main/java/de/jgoldhammer/alfresco/jscript/batchexecuter/ScriptBatchExecuter.java
+++ src/main/java/de/jgoldhammer/alfresco/jscript/batchexecuter/ScriptBatchExecuter.java
@@ -1,5 +1,8 @@
 package de.jgoldhammer.alfresco.jscript.batchexecuter;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.alfresco.repo.batch.BatchProcessor;
 import org.alfresco.repo.jscript.BaseScopableProcessorExtension;
@@ -13,71 +16,76 @@ import org.mozilla.javascript.Scriptable;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.extensions.webscripts.annotation.ScriptClass;
-import org.springframework.extensions.webscripts.annotation.ScriptClassType;
+ import org.springframework.extensions.webscripts.annotation.ScriptClass;
+ import org.springframework.extensions.webscripts.annotation.ScriptClassType;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.WorkProviders.CancellableWorkProvider;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.WorkProviders.CollectionWorkProviderFactory;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.WorkProviders.FolderBrowsingWorkProviderFactory;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.WorkProviders.NodeOrBatchWorkProviderFactory;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.Workers.CancellableWorker;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.Workers.ProcessBatchWorker;
+import de.jgoldhammer.alfresco.jscript.batchexecuter.Workers.ProcessNodeWorker;
 
 /**
  * JavaScript object which helps execute big data changes in Alfresco.
  *
  * The object allows providing a set of nodes to process and a function to run,
- * then splits nodes in batches and executes each de.jgoldhammer.alfresco.jscript.batch in a separate de.jgoldhammer.alfresco.jscript.transaction
+ * then splits nodes in batches and executes each batch in a separate transaction
  * and multiple threads.
  *
  * @author Bulat Yaminov
+ * @author Corentin Roux
  */
 @ScriptClass(types= ScriptClassType.JavaScriptRootObject, code="batchExecuter",
-        help="the root object for the de.jgoldhammer.alfresco.jscript.de interface")
+        help="the root object for the batch interface")
 public class ScriptBatchExecuter extends BaseScopableProcessorExtension implements ApplicationContextAware {
 
     private static final Log logger = LogFactory.getLog(ScriptBatchExecuter.class);
 
-    private ServiceRegistry serviceRegistry;
+    private ServiceRegistry sr;
     private ApplicationContext applicationContext;
 
-    private static ConcurrentHashMap<String, BatchJobParameters> runningJobs = new ConcurrentHashMap<>(10);
-    private static ConcurrentHashMap<String, Pair<WorkProviders.CancellableWorkProvider, Workers.CancellableWorker>>
-            runningWorkProviders = new ConcurrentHashMap<>(10);
+    private static ConcurrentHashMap<String, BatchJobParameters> runningJobs = new ConcurrentHashMap(10);
+    private static ConcurrentHashMap<String, Pair<CancellableWorkProvider, CancellableWorker>>
+            runningWorkProviders = new ConcurrentHashMap(10);
 
     /**
-     * Starts processing an array of objects, applying a function to each object or de.jgoldhammer.alfresco.jscript.batch of objects
+     * Starts processing an array of objects, applying a function to each object or batch of objects
      * within the array.
      *
      * This is a blocking call.
      *
      * @param params processing params, with array stored as 'items' property. See
-     * {@link BatchJobParameters} for all parameters.
+     * {@link src.main.java.de.jgoldhammer.alfresco.repo.jscript.batchexecuter.batchexecuter.BatchJobParameters} for all parameters.
      * @return job ID.
      */
     public String processArray(Object params) {
         BatchJobParameters.ProcessArrayJobParameters job = BatchJobParameters.parseArrayParameters(params);
-        return doProcess(job, WorkProviders.CollectionWorkProviderFactory.getInstance(), job.getItems());
+        return doProcess(job, CollectionWorkProviderFactory.getInstance(), job.getItems());
     }
 
     /**
      * Starts processing a folder and its children recursively, applying a function to each
-     * node or de.jgoldhammer.alfresco.jscript.batch of nodes. Both folders and documents are included.
+     * node or batch of nodes. Both folders and documents are included.
      *
      * This is a blocking call.
      *
      * @param params processing params, with the folder ScriptNode stored as 'root' property. See
-     * {@link BatchJobParameters} for all parameters.
+     * {@link src.main.java.de.jgoldhammer.alfresco.repo.jscript.batchexecuter.batchexecuter.BatchJobParameters} for all parameters.
      * @return job ID.
      */
     public String processFolderRecursively(Object params) {
         BatchJobParameters.ProcessFolderJobParameters job = BatchJobParameters.parseFolderParameters(params);
         return doProcess(job,
-                new WorkProviders.FolderBrowsingWorkProviderFactory(serviceRegistry, getScope(), logger),
+                new FolderBrowsingWorkProviderFactory(sr, getScope(), logger),
                 job.getRoot().getNodeRef());
     }
 
     /**
-     * Get the list of currently executing de.jgoldhammer.alfresco.jscript.jobs.
+     * Get the list of currently executing jobs.
      *
-     * @return collection of de.jgoldhammer.alfresco.jscript.jobs being executed.
+     * @return collection of jobs being executed.
      */
     public Collection<BatchJobParameters> getCurrentJobs() {
         return runningJobs.values();
@@ -99,7 +107,7 @@ public class ScriptBatchExecuter extends BaseScopableProcessorExtension implemen
         // We don't have access to BatchProcessor's executer service,
         // so the only way to cancel is stop giving new work packages
         BatchJobParameters job = runningJobs.get(jobId);
-        Pair<WorkProviders.CancellableWorkProvider, Workers.CancellableWorker> pair = runningWorkProviders.get(jobId);
+        Pair<CancellableWorkProvider, CancellableWorker> pair = runningWorkProviders.get(jobId);
         if (pair != null) {
             boolean workProviderCanceled = pair.getFirst().cancel();
             boolean workerCanceled = pair.getSecond().cancel();
@@ -113,52 +121,52 @@ public class ScriptBatchExecuter extends BaseScopableProcessorExtension implemen
     }
 
     private <T> String doProcess(BatchJobParameters job,
-                                 WorkProviders.NodeOrBatchWorkProviderFactory<T> workFactory,
+                                 NodeOrBatchWorkProviderFactory<T> workFactory,
                                  T data) {
         try {
             /* Process items */
             runningJobs.put(job.getId(), job);
 
             final Scriptable cachedScope = getScope();
-            final String user = AuthenticationUtil.getFullyAuthenticatedUser();
+            final String user = AuthenticationUtil.getRunAsUser();
 
-            RetryingTransactionHelper rth = serviceRegistry.getTransactionService().getRetryingTransactionHelper();
+            RetryingTransactionHelper rth = sr.getTransactionService().getRetryingTransactionHelper();
 
             job.setStatus(BatchJobParameters.Status.RUNNING);
 
             if (job.getOnNode() != null) {
 
                 // Let the BatchProcessor do the batching
-                WorkProviders.CancellableWorkProvider<Object> workProvider =
-                        workFactory.newNodesWorkProvider(data, job.getBatchSize());
-                Workers.ProcessNodeWorker worker = new Workers.ProcessNodeWorker(job.getOnNode(), cachedScope,
-                        user, job.getDisableRules(), serviceRegistry.getRuleService(), logger, this);
+                CancellableWorkProvider<Object> workProvider =
+                        workFactory.newNodesWorkProvider(data, job.getBatchSize(), job.isBrowseSystemNodes());
+                ProcessNodeWorker worker = new ProcessNodeWorker(job.getOnNode(), cachedScope,
+                        user, job.getDisableRules(), sr.getRuleService(), logger, this);
 
-                runningWorkProviders.put(job.getId(), new Pair<WorkProviders.CancellableWorkProvider,
-                                        Workers.CancellableWorker>(workProvider, worker));
+                runningWorkProviders.put(job.getId(), new Pair<CancellableWorkProvider,
+                        CancellableWorker>(workProvider, worker));
 
-                BatchProcessor<Object> processor = new BatchProcessor<>(job.getName(), rth,
+                BatchProcessor<Object> processor = new BatchProcessor(job.getName(), rth,
                         workProvider,
                         job.getThreads(), job.getBatchSize(), applicationContext, logger, 1000);
-                logger.info(String.format("Starting de.jgoldhammer.alfresco.jscript.batch processor '%s' to process %s",
+                logger.info(String.format("Starting batch processor '%s' to process %s",
                         job.getName(), workFactory.describe(data)));
                 processor.process(worker, true);
 
             } else {
 
                 // Split into batches here so that onBatch function can process them
-                WorkProviders.CancellableWorkProvider<List<Object>> workProvider =
-                        workFactory.newBatchesWorkProvider(data, job.getBatchSize());
-                Workers.ProcessBatchWorker worker = new Workers.ProcessBatchWorker(job.getOnBatch(), cachedScope,
-                        user, job.getDisableRules(), serviceRegistry.getRuleService(), logger, this);
+                CancellableWorkProvider<List<Object>> workProvider =
+                        workFactory.newBatchesWorkProvider(data, job.getBatchSize(), job.isBrowseSystemNodes());
+                ProcessBatchWorker worker = new ProcessBatchWorker(job.getOnBatch(), cachedScope,
+                        user, job.getDisableRules(), sr.getRuleService(), logger, this);
 
-                runningWorkProviders.put(job.getId(), new Pair<WorkProviders.CancellableWorkProvider,
-                        Workers.CancellableWorker>(workProvider, worker));
+                runningWorkProviders.put(job.getId(), new Pair<CancellableWorkProvider,
+                        CancellableWorker>(workProvider, worker));
 
-                BatchProcessor<List<Object>> processor = new BatchProcessor<>(job.getName(), rth,
+                BatchProcessor<List<Object>> processor = new BatchProcessor(job.getName(), rth,
                         workProvider,
                         job.getThreads(), 1, applicationContext, logger, 1);
-                logger.info(String.format("Starting de.jgoldhammer.alfresco.jscript.batch processor '%s' to process %s with de.jgoldhammer.alfresco.jscript.batch function",
+                logger.info(String.format("Starting batch processor '%s' to process %s with batch function",
                         job.getName(), workFactory.describe(data)));
                 processor.process(worker, true);
             }
@@ -176,7 +184,7 @@ public class ScriptBatchExecuter extends BaseScopableProcessorExtension implemen
     }
 
     public void setServiceRegistry(ServiceRegistry serviceRegistry) {
-        this.serviceRegistry = serviceRegistry;
+        this.sr = serviceRegistry;
     }
 
     @Override
